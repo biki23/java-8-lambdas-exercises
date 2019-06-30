8f8ec6a4829ae7682e63cb47fb04bed641da2f19
Bikram Kashyap <bkashyap@mdsol.com>
2019-06-30 19:54:40 -0400

    question 1 done

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question1.java
@@ -5,20 +5,25 @@ import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class Question1 {
     public static int addUp(Stream<Integer> numbers) {
-        return Exercises.replaceThisWithSolution();
+        return numbers.reduce(0, (acc, num) -> acc + num);
     }
 
     public static List<String> getNamesAndOrigins(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return artists.stream()
+                .flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
+                .collect(Collectors.toList());
     }
 
     public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
-        return Exercises.replaceThisWithSolution();
+        return input.stream()
+                .filter(album -> album.getTrackList().size() < 4)
+                .collect(Collectors.toList());
     }
 }
